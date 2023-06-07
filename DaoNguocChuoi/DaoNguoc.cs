3ebb386ea2ee6018fa4012b74bcc4630eef73eca
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 15:07:46 +0700

    DungNguyen

---
+++ DaoNguocChuoi/DaoNguoc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaoNguocChuoi
+{
+    internal class DaoNguoc
+    {
+        public static string DaoNguocChuoi_ByFor(string s)
+        {
+            string result = string.Empty;
+            for(int i = s.Length-1; i >=0; i--)
+            {
+               result += s[i];
+            }
+            return result;
+        }
+
+        public static string DaoNguocChuoi_By_ArrayReverse(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            string result = new string(arr);
+            return result;
+        }
+    }
+}
---
+++ DaoNguocChuoi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaoNguocChuoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập chuỗi kí tự: ");
+            string s1 = Console.ReadLine();
+            string s1_daoNguoc = DaoNguoc.DaoNguocChuoi_ByFor(s1);
+            Console.WriteLine("\nDao nguoc theo FOR: " + s1_daoNguoc);
+
+            string s1_daoNguoc2 = DaoNguoc.DaoNguocChuoi_By_ArrayReverse(s1);
+            Console.WriteLine("Dao nguoc theo Array.Reverse: " + s1_daoNguoc2);
+        }
+    }
+}
